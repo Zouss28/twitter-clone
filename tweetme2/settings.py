01149a5ba6c8eae7adb72bb864da97d18a9e4839
Zouss28 <alanjonse2@gmail.com>
2024-01-03 19:14:48 +0200

    Static collected

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tweiter-e7c6d2796ca5.herokuapp.com','localhost']
 LOGIN_URL = "/login"
